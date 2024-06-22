abaac6d098e572a7384fccdc2e5040289c65b474
Bhavana0918 <166234807+Bhavana0918@users.noreply.github.com>
2024-06-22 14:16:56 +0530

    Delete mywebsite directory

--- mywebsite/src/main/resources/static/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('helloBtn').addEventListener('click', function() {
-    fetch('/hello')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('helloMsg').innerText = data;
-        });
-});
